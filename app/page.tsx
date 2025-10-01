8bf6da7d2d6788f5f259407f35cc69d1ccddf751
WebDevHarsha <vasriharsha@gmail.com>
2025-10-01 14:00:08 +0530

    feat(ui): Add basic parallax scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+"use client"
+
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import React from 'react'
 
 export default function page() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Parallax pages={2} style={{ top: '0', left: '0' }}>
+      <ParallaxLayer offset={0} speed={2.5}>
+        <p className='text-3xl'>Parallax</p>
+      </ParallaxLayer>
+      <ParallaxLayer offset={1} speed={2.5}>
+        <p>Parallax</p>
+      </ParallaxLayer>
+    </Parallax>
   )
 }
