6653702d0ec2be0bee0c919812736c498b77199e
WebDevHarsha <vasriharsha@gmail.com>
2025-10-01 18:57:47 +0530

    feat(homepage): Add parallax visuals and theme setup

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,60 @@ import React from 'react'
 
 export default function page() {
   return (
-    <Parallax pages={2} style={{ top: '0', left: '0' }}>
-      <ParallaxLayer offset={0} speed={2.5}>
-        <p className='text-3xl'>Parallax</p>
-      </ParallaxLayer>
-      <ParallaxLayer offset={1} speed={2.5}>
-        <p>Parallax</p>
-      </ParallaxLayer>
-    </Parallax>
+  <div className='w-screen min-h-screen bg-[#f6aa1cff]' >
+      <Parallax pages={2}  style={{ top: '0', left: '0'}} >
+        <ParallaxLayer
+          offset={0}
+          speed={0.7}
+          style={{
+            backgroundImage: 'url(/spider.gif)',
+            backgroundSize: '22%',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'left top',
+            backgroundColor: 'transparent',
+            position: 'relative',
+            zIndex: 2,
+          }}
+        />
+
+        <ParallaxLayer
+          offset={0}
+          speed={0.7}
+          style={{
+            backgroundImage: 'url(/bats.png)',
+            backgroundSize: '28%',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'right top',
+            backgroundColor: 'transparent',
+            zIndex: 1,
+          }}
+        />
+        <ParallaxLayer
+          offset={0}
+          speed={3}
+          style={{
+            backgroundImage: 'url(/house.png)',
+            backgroundSize: '18%',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'left bottom',
+            backgroundColor: 'transparent',
+            zIndex: 1,
+          }}
+        />
+
+        <ParallaxLayer
+          offset={0}
+          speed={2.5}
+          // style={{ position: 'relative', zIndex: 10 }}
+        >
+          <div className='flex justify-center items-center h-screen'>
+            <p className='text-3xl'>Welcome to my Portfolio</p>
+          </div>
+        </ParallaxLayer>
+
+
+        
+      </Parallax>
+    </div>
   )
 }
